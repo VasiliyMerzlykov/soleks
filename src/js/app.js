91a2f204527dc04d62b21ac8246659f3d31f44c4
Vasiliy Merzlyakov <V.Merov@yandex.ru>
2020-12-04 17:02:14 +0500

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,13 @@
+const sol = document.querySelectorAll('.linc_nav');
+const main = document.querySelectorAll('main > div');
+
+[...sol].map((item, index) => {
+  item.addEventListener('click', () => {
+    [...main].find((elem) => {
+      elem.classList.remove('activ');
+      main[index].classList.add('activ');
+      return true;
+    });
+  });
+  return true;
+});
